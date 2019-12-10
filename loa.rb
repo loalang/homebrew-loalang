0b49153b8e32f26c1ff6c293b4c2bea2ce37b105
Emil Broman <emil@emilbroman.me>
2019-12-10 21:20:58 +0000

    Update loa.rb

--- loa.rb
+++ loa.rb
@@ -10,4 +10,7 @@ class Loa < Formula
     url "https://storage.googleapis.com/dart-archive/channels/stable/release/2.6.1/sdk/dartsdk-macos-ia32-release.zip"
     sha256 "161122c60c89db5049a7617630d7a492cdb6bb2e73b23daf49a16bd9e2c0c52d"
   end
+  
+  def install
+  end
 end
