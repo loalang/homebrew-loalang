8a967e204ad9cea0de2a017f050c934c2e31452c
Emil Broman <emil@emilbroman.me>
2019-12-10 21:08:59 +0000

    Add mocked formula (w/ Dart artifacts...)

---
+++ loa.rb
@@ -0,0 +1,13 @@
+class Loa < Formula
+  desc "Loa Programming Language Compiler Toolchain"
+  homepage "https://loalang.xyz"
+  
+  version "0.0.1"
+  if Hardware::CPU.is_64_bit?
+    url "https://storage.googleapis.com/dart-archive/channels/stable/release/2.6.1/sdk/dartsdk-macos-x64-release.zip"
+    sha256 "3063a3151e91367fff95f63c781519a54674cc5e8b9bc847e2c6de96ed611a14"
+  else
+    url "https://storage.googleapis.com/dart-archive/channels/stable/release/2.6.1/sdk/dartsdk-macos-ia32-release.zip"
+    sha256 "161122c60c89db5049a7617630d7a492cdb6bb2e73b23daf49a16bd9e2c0c52d"
+  end
+end
