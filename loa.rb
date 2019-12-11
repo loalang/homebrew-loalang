645a70e67848dfa7326842792c9caf6c06c58a8d
Emil Broman <emil@emilbroman.me>
2019-12-11 16:47:51 +0000

    Update loa.rb

--- loa.rb
+++ loa.rb
@@ -4,11 +4,11 @@ class Loa < Formula
   
   version "0.0.2"
   if OS.mac?
-    url "https://storage.googleapis.com/loalang-releases/0.1.0_x86_64-macos.tar.gz"
-    sha256 "3896df9eeaf9f7ab7219f92bc8295adbd55559c417cc9462f1f7de64bbf1cfc4"
+    url "https://storage.googleapis.com/loalang-releases/0.1.1_x86_64-macos.tar.gz"
+    sha256 "7212d217a8fdab5bfb96199db0772a65931b25ed5a11a1f7121dc3ee32e20a40"
   else
-    url "https://storage.googleapis.com/loalang-releases/0.1.0_x86_64-linux.tar.gz"
-    sha256 "ace3e0828c93a08a70e09458d6259a148c747c633857c1216e23bdad3d2e1455"
+    url "https://storage.googleapis.com/loalang-releases/0.1.1_x86_64-linux.tar.gz"
+    sha256 "1d43823b1f8e17a73fedad904906860f55dd32ef5806f292003685700d265c11"
   end
   
   def install
