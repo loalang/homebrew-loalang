f7deae58011fd1701f034fc09bdc4eac2961986c
Emil Broman <emil.broman@eqtpartners.com>
2020-01-09 21:17:12 +0100

    0.1.8

--- loa.rb
+++ loa.rb
@@ -1,22 +1,22 @@
 class Loa < Formula
   desc "Loa Programming Language Compiler Toolchain"
   homepage "https://loalang.xyz"
-  
-  version "0.1.7"
+
+  version "0.1.8"
   if OS.mac?
     url "https://cdn.loalang.xyz/#{version}_x86_64-macos.tar.gz"
-    sha256 "6a189fa277d7265c8df3143f62eefc35df9167de856d0e6507c127bfadd43b1b"
+    sha256 "c83f7e9c6209eda42e645eb7da0d71cebc96f58195ba92cb943e2ad6176a5771"
   else
     url "https://cdn.loalang.xyz/#{version}_x86_64-linux.tar.gz"
-    sha256 "5209a08f20e435e77b6f02c7350b9ebf2e296a611edb3e922ce4c1d9f7486392"
+    sha256 "f2e98efafcaf83d1fb8cd56e91e00dc1793a5b487fbfbe89bd10f79460e7c9c9"
   end
-  
+
   def install
     bin.install "bin/loa"
     bin.install "bin/loavm"
-    
+
     lib.install "lib/loa"
-    
+
     (var/"log").mkpath
     (var/"log").install "var/log/loa.log"
   end
