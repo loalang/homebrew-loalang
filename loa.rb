3147e2c85b14d67967497981c37037367c72d52c
Emil Broman <emil.broman@eqtpartners.com>
2020-02-25 22:55:20 +0100

    0.1.12

--- loa.rb
+++ loa.rb
@@ -2,13 +2,13 @@ class Loa < Formula
   desc "Loa Programming Language Compiler Toolchain"
   homepage "https://loalang.xyz"
 
-  version "0.1.11"
+  version "0.1.12"
   if OS.mac?
     url "https://cdn.loalang.xyz/#{version}_x86_64-macos.tar.gz"
-    sha256 "a0a0f1202abf22f1fe079cd94da8b586f4cd9c1fc43a23c0573d0631ed4af2e7"
+    sha256 "9be7bc947b830435c96f303356546cab35c142d6232b72ae58b491eb2ebdcc59"
   else
     url "https://cdn.loalang.xyz/#{version}_x86_64-linux.tar.gz"
-    sha256 "82b12cb99a2e732ec1b32c22a6b18ce289e3058afde72ab574f4b9fa2fcb35d0"
+    sha256 "d9c0c1975ff381b854ff171bcd28e1b180e09c9744cb7c70d7d45f9837d32ada"
   end
 
   def install
