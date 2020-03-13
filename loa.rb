0d6ac0d1cd4cb6c0d4b81741610bdb2e43fa2847
Publish Workflow <cd@loalang.xyz>
2020-03-13 02:03:52 +0000

    Publish 0.1.17

--- loa.rb
+++ loa.rb
@@ -5,10 +5,10 @@ class Loa < Formula
   version "0.1.17"
   if OS.mac?
     url "https://cdn.loalang.xyz/#{version}-x86_64-macos.tar.gz"
-    sha256 "e686e8fc78a469a4227017f3a490e4e09180dbaf7b1bfef2789d244f0bfe5e40"
+    sha256 "9b877f61529d0c3a58420d30ead03dd9d976751e98bf29de147fefcd66ee9e19"
   else
     url "https://cdn.loalang.xyz/#{version}-x86_64-linux.tar.gz"
-    sha256 "0e27072c8f569fe47f73cb558602b66418102354c9361618787feb87052d6fe8"
+    sha256 "1e904e908ea79d31d8f6d6d65d7da36b1193410e74875acb50a42fe5027545dc"
   end
 
   def install
