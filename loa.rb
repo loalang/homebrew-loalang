b5f6f077b34317ccc6421d4e39379becfddd5303
Publish Workflow <cd@loalang.xyz>
2020-03-14 10:30:28 +0000

    Publish 0.1.19

--- loa.rb
+++ loa.rb
@@ -2,13 +2,13 @@ class Loa < Formula
   desc "Loa Programming Language Compiler Toolchain"
   homepage "https://loalang.xyz"
 
-  version "0.1.18"
+  version "0.1.19"
   if OS.mac?
     url "https://cdn.loalang.xyz/#{version}-x86_64-macos.tar.gz"
-    sha256 "d0328c0555bb282a4050c407c892f293dfc0ea23aa40fa04cbfe56774ee7eb00"
+    sha256 "d1d1e92bfddd208243fc54605cdd7da024fe484f38e3632644879b6f69ce440d"
   else
     url "https://cdn.loalang.xyz/#{version}-x86_64-linux.tar.gz"
-    sha256 "04e39e77ab1b95e11eaf9b52f9e83dac13a60f57ab87d1451f8a34d3248a12d2"
+    sha256 "e36ea660ba259f4d877219bf38f6793c253f1a2c6fcf25b698236eb073bd257c"
   end
 
   def install
